aa96f94795977be90df3f37281657ff0f19fa4da
shravani <164602979+shravani2138@users.noreply.github.com>
2024-07-27 11:25:28 +0530

    matrixoperation.java

--- matrixoperation.java
+++ matrixoperation.java
@@ -8,82 +8,78 @@ class matrix
 		int b[][] = new int [2][2];
 		Scanner sc = new Scanner(System.in);
 
-void getmatrixele1()
+void getmatrixele()
 	  {
-		System.out.print("enter first matrix ");
+		System.out.print("enter first matrix element :");
 		for(int i=0;i<2;i++)
 		   { 
 			for(int j=0;j<2;j++)
 			   {
-				System.out.print(a[i][j] + " " );
+				a[i][j] = sc.netInt();
+			   }
                      }
-               }
-		a = sc.Scanner.nextInt();
-        } 
-void getmatrixele2()
-	  {
-		System.out.print("enter second matrix ");
+         }
+		System.out.print("enter second matrix element :");
 		for(int i=0;i<2;i++)
 		   { 
 			for(int j=0;j<2;j++)
 			   {
-				System.out.print(b[i][j] + " " );
-                     }
-               }
-		b = sc.Scanner.nextInt();
-        } 
+				b[i][j] = sc.netInt();
+                   	   }
+                    }
 void addmatrix()
 	  {
+		System.out.print("\n addition of matrix :");
 		for(int i=0;i<2;i++)
 		   { 
 			for(int j=0;j<2;j++)
 			   {
-				int c = a[i][j] + b[i][j];
-				System.out.print("addition of matrices " + c);
+				int c[i][j] = a[i][j] + b[i][j];
+				System.out.print(c +" ");
                      }
+		System.out.println();
                }
-		c = sc.Scanner.nextInt();
         } 
 void submatrix()
 	  {
+		System.out.print("\n subtraction of matrix :");
 		for(int i=0;i<2;i++)
 		   { 
 			for(int j=0;j<2;j++)
 			   {
-				int d = a[i][j] - b[i][j];
-				System.out.print("subtraction of matrices " + d);
+				int d[i][j] = a[i][j] - b[i][j];
+				System.out.print(d + " ");
                      }
                }
-		d = sc.Scanner.nextInt();
         } 
 void mulmatrix()
-	  {
+	  {	
+		int e[][] = int[2][2];
+		System.out.print("\n multiplication of matrix :");
 		for(int i=0;i<2;i++)
 		   { 
 			for(int j=0;j<2;j++)
 			   {
+				e[i][j] = 0;
 				for(int k=0;k<2;k++)
 		   
 			          {
-				        int e = a[i][k] * b[k][j];
-				        System.out.print("multiplication of matrices " + e);
-				    }
-                     }
-               }
-		e = sc.Scanner.nextInt();
-        } 
-class matrixoperation
+				        e[i][j]+ = a[i][k] * b[k][j];
+				  }
+				  System.out.print(e[i][j] + " ");
+                           }
+			   System.out.println();
+                   }
+          } 
+class Matrixoperation
 {
 	public static void main(string[] args)
        {
-		matix m = new matrix();
+		matix M = new Matrix();
 
-		m.getmatrixele1();
-		m.getmatrixele2();
-		m.addmatrix();
-		m.submatrix();
-		m.mulmatrix();
+		M.getmatrixele();
+		M.addmatrix();
+		M.submatrix();
+		M.mulmatrix();
 	}
-}
-}
 }
