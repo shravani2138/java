1323f65799d3d4ea8ed9fa5f2f7688d85109f2f5
shravani <164602979+shravani2138@users.noreply.github.com>
2024-07-29 10:35:28 +0530

    Add files via upload

---
+++ perimeterofEquiTri.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class PerimeterOfEquilateralTriangle 
+{
+   public static void main(String args[]) 
+    {   
+         Scanner s= new Scanner(System.in);
+         System.out.println("Enter the side of the Triangle:");
+         
+	 double a= s.nextDouble();
+ 
+         double  perimeter=3*a;
+      
+         System.out.println("perimeter of Triangle is: " + perimeter);      
+   }
+}
